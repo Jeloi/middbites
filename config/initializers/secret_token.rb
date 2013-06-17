aa155575ed6a1b4513cf06c1912f6cc32d14e6ca
Jeloi <jeremyho19@gmail.com>
2013-06-16 18:14:46 -0700

    Initial commit with gemfile and rspec install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Middbites::Application.config.secret_key_base = '67130d42475bb861d6dabf84a68bd6bc262becab89b3df035dc127a9719a36cd26198889fbc1b5649957b7102510a5ff78afd6329e1ed49d41e0f6542e9d18f7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Middbites::Application.config.session_store :cookie_store, key: '_middbites_session'
