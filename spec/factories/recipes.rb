71144fe21c87726e6f0a11e1097dc8a778d27284
Jeloi <jeremyho19@gmail.com>
2013-08-30 23:22:45 -0700

    view work for recipe#show. created factories, seeds for ingredients and items

--- spec/factories/recipes.rb
+++ spec/factories/recipes.rb
@@ -3,5 +3,15 @@ FactoryGirl.define do
     title "The Classic"
     blurb "A sandwich so good you'll never forget it"
   	directions "Get two pieces of bread. Spread peanut butter on one, jelly on the other. Stick the two pieces together"
+
+  	factory :recipe_with_ingredients do
+		
+  		after(:build) do |recipe|
+  			[:jelly, :peanut_butter, :bread].each do |item|
+  				recipe.ingredients << Ingredient.new(quantity: "1", item: FactoryGirl.build(item))
+  			end
+  		end
+
+  	end	
   end
 end
