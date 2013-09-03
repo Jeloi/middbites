c72383cfd8616ba2fbda6c52560dd8461b3fb635
Jeloi <jeremyho19@gmail.com>
2013-09-02 21:42:58 -0700

    dynamic adding and removing ingredients. still need to select through single dropdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def add_fields_helper(form_builder, association)
+    new_object = form_builder.object.class.reflect_on_association(association).klass.new
+    fields = form_builder.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(association.to_s.singularize + "_fields", :f => builder)
+    end
+    # link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
+  end
 end
