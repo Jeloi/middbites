cd6fffb89d96a3c892e91c13e94b15d001208074
Jeloi <jeremyho19@gmail.com>
2013-10-22 23:45:43 -0700

    made menus page sliders responsive through javascript listener on window resize, defiend in menus.js

--- app/assets/javascripts/menus.js
+++ app/assets/javascripts/menus.js
@@ -1,43 +1,134 @@
 
+// Global Variable to hold slider objects, and their pages
+var sliders_array = [];
+var pager_array = [];
+
+// screen width variables for number of slides
+var oneSlide = 515;
+var twoSlides = 800;
+// var 3slides = 800;
+
+// Initial Sliders set and stored in array. Respective pagers are stored in an array in same order.
+menuSlider = function() {
+	sliders_array = [];
+	var numSliders = $('#sliders_counter').data("numsliders");
+	if ($(window).width() < oneSlide) {
+		for (var i = 0; i < numSliders; i++) {
+			var slider = '#bxslider' + i;
+			var pager = '#bxpager' + i;
+			var slider_obj = $(slider).bxSlider({
+				slideWidth: 600,
+				minSlides: 1,
+				maxSlides: 1,
+				moveSlides: 1,
+				infiniteLoop: true,
+				controls: false,
+				pagerCustom: pager,
+				slideMargin: 10
+			});
+			sliders_array.push(slider_obj); //add the slider_obj to the array
+			pager_array.push(pager); //add the slider_obj to the array
+		};
+	} else if ($(window).width() < 800) {
+		for (var i = 0; i < numSliders; i++) {
+			var slider = '#bxslider' + i;
+			var pager = '#bxpager' + i;
+			var slider_obj = $(slider).bxSlider({
+				slideWidth: 400,
+				minSlides: 2,
+				maxSlides: 2,
+				moveSlides: 1,
+				infiniteLoop: true,
+				controls: false,
+				pagerCustom: pager,
+				slideMargin: 10
+			});
+			sliders_array.push(slider_obj); //add the slider_obj to the array
+			pager_array.push(pager); //add the slider_obj to the array
+		};
+	} else {
+		for (var i = 0; i < numSliders; i++) {
+			var slider = '#bxslider' + i;
+			var pager = '#bxpager' + i;
+			var slider_obj = $(slider).bxSlider({
+				slideWidth: 400,
+				minSlides: 3,
+				maxSlides: 3,
+				moveSlides: 1,
+				infiniteLoop: true,
+				controls: false,
+				pagerCustom: pager,
+				slideMargin: 10
+			});
+			sliders_array.push(slider_obj); //add the slider_obj to the array
+			pager_array.push(pager); //add the slider_obj to the array
+		};
+	}
+};
+
+
+
+
+
+
 var id; // Global variable
 var screen_width = $(window).width();
+
+
+// Perform resize functionality after its done resizing
+function doneResizing(){
+	// if the width of the window changed only
+	if ($(window).width() != screen_width) {
+		if ($(window).width() < oneSlide) {
+			for (var i = 0; i < sliders_array.length; i++) {
+				sliders_array[i].reloadSlider({
+					slideWidth: 600,
+					minSlides: 1,
+					maxSlides: 1,
+					moveSlides: 1,
+					infiniteLoop: true,
+					controls: false,
+					pagerCustom: pager_array[i],
+					slideMargin: 10
+				});
+			}
+		} else if ($(window).width() < twoSlides) {
+			for (var i = 0; i < sliders_array.length; i++) {
+				sliders_array[i].reloadSlider({
+					slideWidth: 400,
+					minSlides: 2,
+					maxSlides: 2,
+					moveSlides: 1,
+					infiniteLoop: true,
+					controls: false,
+					pagerCustom: pager_array[i],
+					slideMargin: 10
+				});
+			}
+		} else {
+			for (var i = 0; i < sliders_array.length; i++) {
+				sliders_array[i].reloadSlider({
+					slideWidth: 400,
+					minSlides: 3,
+					maxSlides: 3,
+					moveSlides: 1,
+					infiniteLoop: true,
+					controls: false,
+					pagerCustom: pager_array[i],
+					slideMargin: 10
+				});
+			}
+		}
+		screen_width = $(window).width();
+	};
+}
+
+// Listeners
+$(document).ready(menuSlider);
+$(document).on('page:load', menuSlider);	// for turbolinks
 $(window).resize(function() {
     clearTimeout(id);
     id = setTimeout(
     	doneResizing, 800);
     
 });
-
-// Perform resize functionality after its done resizing
-function doneResizing(){
-	// if the width of the window changed only
-	if ($(window).width() != screen_width) {
-		screen_width = $(window).width();
-	};
-	console.log("Window Resized");
-}
-
-var menuSlider;
-menuSlider = function() {
-	var numSliders = $('#sliders_counter').data("numsliders");
-	for (var i = 0; i < numSliders; i++) {
-		var slider = '#bxslider' + i;
-		var pager = '#bxpager' + i;
-		$(slider).bxSlider({
-			slideWidth: 320,
-			minSlides: 1,
-			maxSlides: 1,
-			moveSlides: 1,
-			infiniteLoop: false,
-			controls: false,
-			pagerCustom: pager,
-			slideMargin: 10
-		});
-	};
-
-};
-
-
-
-$(document).ready(menuSlider);
-$(document).on('page:load', menuSlider);
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -9,6 +9,6 @@ class PagesController < ApplicationController
   def menus
 
 		@menus_hash = MenuScraper.scrape_midd_menus("http://menus.middlebury.edu", params[:date], params[:sort_by])
-		# logger.debug { @menus_hash }
+		logger.debug { @menus_hash }
   end
 end
