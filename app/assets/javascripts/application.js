e2dcba860175870b423a2cba4e5db2c2da53c04c
Jeloi <jeremyho19@gmail.com>
2013-12-30 14:11:55 -0500

    complete redesign. styling for 3 types of views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,3 +39,50 @@
 // 	console.log("Window Resized");
 // }
 $(function(){ $(document).foundation(); });
+
+$(function () {
+
+	// Sticky Navigation
+	// grab the initial top offset of the navigation 
+	var sticky_navigation_offset_top = $('#sticky_navigation').offset().top;
+	 
+	// our function that decides weather the navigation bar should have "fixed" css position or not.
+	var sticky_navigation = function(){
+	    var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+	     
+	    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+	    // otherwise change it back to relative
+	    if (scroll_top > sticky_navigation_offset_top) { 
+	        $('#sticky_navigation').css({ 'position': 'fixed', 'top':0, 'left':0 });
+	    } else {
+	        $('#sticky_navigation').css({ 'position': 'relative' }); 
+	    }   
+	};
+	 
+	// run our function on load
+	sticky_navigation();
+	 
+	// and run it again every time you scroll
+	$(window).scroll(function() {
+	     sticky_navigation();
+	});
+
+	// Vertically Center Content
+	function verticalCenter() {
+		if ($(window).width() >= 500) {
+			$('.vert-align').each(function() {
+				var $itemHeight = $(this).parent().outerHeight();
+				var $itemTitleHeight = $(this).outerHeight();
+				var $marginTop = ($itemHeight - $itemTitleHeight) / 2;
+				$(this).css('margin-top', $marginTop + "px");
+			});
+		} else {
+			$('.vert-align').css('margin-top', "0px");
+		};
+	}
+
+	if($('.recipe-item').length) {
+		verticalCenter();
+		$(window).on('load resize facetwp-loaded', verticalCenter);
+	}
+});
