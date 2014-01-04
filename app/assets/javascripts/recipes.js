e88643cdcbaaafa5c94082317cf6c6fc44733ba2
Jeloi <jeremyho19@gmail.com>
2014-01-04 05:28:38 -0500

    fade in visibility for new recipe items

--- app/assets/javascripts/recipes.js
+++ app/assets/javascripts/recipes.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
 
+	// Show all invisible recipe items upon load
+	$('.invisible').css({opacity: 0.0, visibility: "visible"}).animate({opacity: 1.0}).removeClass('invisible');
+	
 	// "Load More" ajax functionality
 	$('.recipe-listings').on('click', '#load_more', function(event) {
 		event.preventDefault();
 		url	= $('.pagination .next_page a').attr('href');
-		$('#load_more').text('Loading...')
+		$('#load_more').text('Loading...');
+		setTimeout(5);
 		$.getScript(url);
 	});
 	
