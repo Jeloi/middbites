c069db793cd9aa1da4fe2c95f0c0bf2ad88e49d6
Jeloi <jeremyho19@gmail.com>
2014-02-07 01:22:36 -0500

    added ui fixes, back-to-top button

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -50,24 +50,24 @@ $(function () {
 
 	// Sticky Navigation
 	// grab the initial top offset of the navigation 
-	var sticky_navigation_offset_top = $('#sticky_navigation').offset().top;
+	// var sticky_navigation_offset_top = $('#sticky_navigation').offset().top;
 	 
-	// our function that decides weather the navigation bar should have "fixed" css position or not.
-	var sticky_navigation = function(){
-	    var scroll_top = $(window).scrollTop(); // our current vertical position from the top
+	// // our function that decides weather the navigation bar should have "fixed" css position or not.
+	// var sticky_navigation = function(){
+	//     var scroll_top = $(window).scrollTop(); // our current vertical position from the top
 	     
-	    // if we've scrolled more than the navigation, change its position to fixed to stick to top,
-	    // otherwise change it back to relative
-	    if (scroll_top > sticky_navigation_offset_top) { 
-	        // $('#sticky_navigation').css({ 'position': 'fixed', 'top':0, 'left':0 });
-	        $('#sticky_navigation').addClass('fixed');
-	        $('.fixed-appear').fadeIn('400');
-	    } else {
-	        $('#sticky_navigation').removeClass('fixed');
-	        // $('#sticky_navigation').css({ 'position': 'relative' }); 
-	        $('.fixed-appear').hide();
-	    }   
-	};
+	//     // if we've scrolled more than the navigation, change its position to fixed to stick to top,
+	//     // otherwise change it back to relative
+	//     if (scroll_top > sticky_navigation_offset_top) { 
+	//         // $('#sticky_navigation').css({ 'position': 'fixed', 'top':0, 'left':0 });
+	//         $('#sticky_navigation').addClass('fixed');
+	//         $('.fixed-appear').fadeIn('400');
+	//     } else {
+	//         $('#sticky_navigation').removeClass('fixed');
+	//         // $('#sticky_navigation').css({ 'position': 'relative' }); 
+	//         $('.fixed-appear').hide();
+	//     }   
+	// };
 	 
 	// run our function on load
 	sticky_navigation();
@@ -95,9 +95,17 @@ $(function () {
 	   $('#secondHeader .menu-button').toggleClass("menu-button-active");
 	});
 
-
+	//Hide search modal, didn't work
 	$('.reveal-modal-bg').on('touchstart click', function() {
 	   $('#search-modal').foundation('reveal', 'close');
+	});
+
+	// Back to top scrolling
+	$('a.back-to-top').click(function (e) {
+		e.preventDefault();
+		$('html,body').animate({
+		scrollTop: 0}, 500);
+		return	false;
 	});
 
 });
