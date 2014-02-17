759c29f75b62ba8256bc0032867f40e9b22a135a
Jeloi <jeremyho19@gmail.com>
2014-02-17 01:49:36 -0500

    styled flash, aded ui

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -9,6 +9,11 @@ $(function(){
 		close_on_background_click: true
 	});
 
+	$('.flash .fa-times').click(function(event) {
+		$(this).closest('.flash').fadeOut('400');
+		console.log("got here!");
+	});
+
 });
 
 // Search modal effects
